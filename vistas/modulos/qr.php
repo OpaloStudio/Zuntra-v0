881a9c3a55061094decd052a1972082281b41d3b
RobertoVz <roberto@opalo.studio>
2019-07-12 17:11:40 -0500

    lista la vista de los qr

--- vistas/modulos/qr.php
+++ vistas/modulos/qr.php
@@ -1,3 +1,26 @@
-<div class="contenedorDocumentos">
-  <img class="imgQr" src="vistas/img/pix3.png">
+<div class="contenedorQr">
+<div class="divEditar"><i class="far fa-edit"></i></div>
+  <img class="imgQr" src="vistas/img/pix3.png"><br>
+  <div class="row">
+  <hr>
+  <div class="col-4 flecQr"><p>Reservación<br>3/5</p></div>
+  <div class="col-4 flecQr borde"><p>Sábado<br>12/06/19 - 10:30pm</p></div>
+  <div class="col-4 flecQr borde"><p>Nº de reservación<br>000105</p></div>
+  <hr>
+  </div>
+
+  <dl class="listaQr">
+  <dd>-Para validar tu reservación es importante que lleves tu QR de
+  forma digital o impreso</dd>
+  <dd>-Las reservaciones no son transferibles</dd>
+  <dd>-En la entrada se solicitara una identificación oficial</dd>
+  <dd>-Todas las reservaciones vencen a las 11:30pm</dd>
+  <dd>-Aplican restricciones</dd>
+  <dd>-Consulta todas tus dudas enviándonos un mensaje privado</dd>
+  <dd>-No valida en eventos especiales</dd> 
+  </dl>
+  <div class="zonaBtn">
+    <button type="submit" class="btn btnOscuro btnDescargar">Descargar Imágenes (xx)</button>
+    <button type="submit" class="btn btnOscuro btnShare"><i class="fas fa-share"></i></button>
+  </div>
 </div>
