597e0d6486a1a0b343f48e03e05ddb42f2cb391c
RobertoVz <roberto@opalo.studio>
2019-07-12 14:55:23 -0500

    listo la parte de bloquear usuario

---
+++ controladores/controlador.swipe.php
@@ -0,0 +1,5 @@
+<script>
+function abrirModal(){
+    
+}
+</script>
--- vistas/modulos/swipe.php
+++ vistas/modulos/swipe.php
@@ -11,7 +11,7 @@
       <i class="fas fa-ellipsis-h"></i>
     </button>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-      <a class="dropdown-item" href="?cerrar=1">Bloquear</a>
+      <button type="button" class="btn btnModal center" data-toggle="modal" data-target="#exampleModal">Bloquear</button>
 
     </div>
 
@@ -21,6 +21,55 @@
   <div class="row imgSwipe">
     <div class="col-12">
     <div class="divNombre">
+
+    
+
+<!-- Modal -->
+<div class="modal fade elModal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+      <div class="content">
+  <div class="box">
+    <p>
+      <input type="checkbox" id="c1" name="cb">
+      <label for="c1">No especificar</label>
+    </p>
+    <p>
+      <input type="checkbox" id="c2" name="cb">
+      <label for="c2">Comportamiento fuera de lugar</label>
+    </p>
+    <p>
+      <input type="checkbox" id="c3" name="cb">
+      <label for="c3">Es muy insistente</label>
+    </p>
+
+    <p>
+      <input type="checkbox" id="c4" name="cb">
+      <label for="c4">Hay una relación pasada</label>
+    </p>
+  </div>
+</div>
+        
+        <div class="form-group">
+    <label for="exampleFormControlTextarea1">Otra razón o queja</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+      </div>
+      </form>
+      <div class="divSubmit">
+        <button type="button" class="btn btnBloquear">Bloquear</button>
+      </div>
+    </div>
+  </div>
+</div>
     <h2>Rodrigo Rosas</h2>
     <h6>24 años - citas</h6>
     </div>
