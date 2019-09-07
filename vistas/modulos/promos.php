9dce886429a7f85942cf73d9a473831156db0fb1
RobertoVz <roberto@opalo.studio>
2019-09-06 21:12:12 -0500

    listo el rediseno de la seccio de promos

--- vistas/modulos/promos.php
+++ vistas/modulos/promos.php
@@ -1,16 +1,56 @@
 <div class="contenedorPromos">
-<nav class="navbar fixed-top navbar-dark navInbox">
+  <nav class="navbar fixed-top navbar-dark navInbox">
     <div class="iconos">
       <a href="?page=13"> <i class="fas fa-arrow-left"></i></a>
     </div>
-  </nav>
-<div class="row">
-    <div class="col-12 slidePromo"><a href="?page=3">Promos</a></div>
-  </div>
-  <div class="row">
-    <div class="col-12 slidePromo"><a href="?page=3">Promos</a></div>
-  </div>
-  <div class="row">
-    <div class="col-12 slidePromo"><a href="?page=3">Promos</a></div>
-  </div>
+  </nav><br><br><br><br>
+  <div class="container">
+    <div class="row">
+      <div class="divPromo">
+        <h1 class="text-center">Promos</h1>
+      </div>
+      <div class="col-12 zonaPromos">
+        <h1>Jueves</h1>
+        <img class="promo" src="vistas/img/promo2.jpg">
+        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 zonaPromos">
+      <h1>Viernes</h1>
+      <img class="promo" src="vistas/img/promo3.jpg">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 zonaPromos">
+      <h1>Sábado</h1>
+      <img class="promo" src="vistas/img/promo4.jpg">
+      </div>
+    </div>
+  </div><br><br>
+
+  <div class="container">
+    <div class="row">
+      <div class="divPromo">
+        <h1 class="text-center">Eventos</h1>
+      </div>
+      <div class="col-12 zonaPromos">
+        <h1>Jueves</h1>
+        <img class="promo" src="vistas/img/promo2.jpg">
+        
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 zonaPromos">
+      <h1>Viernes</h1>
+      <img class="promo" src="vistas/img/promo3.jpg">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 zonaPromos">
+      <h1>Sábado</h1>
+      <img class="promo" src="vistas/img/promo4.jpg">
+      </div>
+    </div>
+  </div><br><br>
 </div>
