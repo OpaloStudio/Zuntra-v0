491540f05c6af651a88f2da6269185c5568148d3
RobertoVz <roberto@opalo.studio>
2019-09-06 20:19:27 -0500

    listo la parte del logscreen y el home

--- vistas/modulos/logScreen.php
+++ vistas/modulos/logScreen.php
@@ -1,4 +1,8 @@
 <div class="contenedorLogScreen">
+  <a href="?page=13">
+    <img class="logoLog" src="vistas/img/logo.png">
+  </a>
   <button class="btnOscuro"  onclick="irLogin()">Login</button><br><br>
-  <button class="btnOscuro"  onclick="irRegistro()">Registro</button>
+  <button class="btnOscuro"  onclick="irRegistro()">Registro</button><br><br>
+  <button class="btnOscuro"  onclick="irGuest()">Sesión de Invitado</button>
 </div>
