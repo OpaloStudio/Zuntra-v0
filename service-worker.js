91390aa99605c2d41bcde6fe9d5dd44dbeab8b56
RobertoVz <roberto@opalo.studio>
2019-09-06 21:29:54 -0500

    listo

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,52 @@
 const cacheName = 'static-shell-v1';
 const resourcesToPrecache = [
-    '/',
-    'vistas/modulos/home.php',
-    'vistas/modulos/cambiar.php',
-    'vistas/modulos/chat.php',
-    'vistas/modulos/escaner.php',
-    'vistas/modulos/inbox.php',
-    'vistas/modulos/login.php',
-    'vistas/modulos/logScreen.php',
-    'vistas/modulos/perfil.php',
-    'vistas/modulos/promos.php',
-    'vistas/modulos/qr.php',
-    'vistas/modulos/recuperar.php',
-    'vistas/modulos/register.php',
-    'vistas/modulos/reservar.php',
-    'vistas/modulos/swipe.php'
+'vistas/modulos/cambiar.php',
+'vistas/modulos/chat.php',
+'vistas/modulos/escaner.php',
+'vistas/modulos/inbox.php',
+'vistas/modulos/login.php',
+'vistas/modulos/logScreen.php',
+'vistas/modulos/perfil.php',
+'vistas/modulos/promos.php',
+'vistas/modulos/qr.php',
+'vistas/modulos/recuperar.php',
+'vistas/modulos/register.php',
+'vistas/modulos/reservar.php',
+'vistas/modulos/swipe.php',
+'controladores/controlador.cambiar.php',
+'controladores/controlador.chat.php',
+'controladores/controlador.escaner.php',
+'controladores/controlador.home.php',
+'controladores/controlador.inbox.php',
+'controladores/controlador.login.php',
+'controladores/controlador.logScreen.php',
+'controladores/controlador.perfil.php',
+'controladores/controlador.promos.php',
+'controladores/controlador.qr.php',
+'controladores/controlador.recuperar.php',
+'controladores/controlador.register.php',
+'controladores/controlador.reservar.php',
+'controladores/controlador.swipe.php',
+'modelos/modelo.administrar.php',
+'modelos/modelo.bitacoras.php',
+'modelos/modelo.bloqueo.php',
+'modelos/modelo.calendario.php',
+'modelos/modelo.cambiar.php',
+'modelos/modelo.conexion.php',
+'modelos/modelo.configuracion.php',
+'modelos/modelo.dashboard.php',
+'modelos/modelo.documentos.php',
+'modelos/modelo.escaner.php',
+'modelos/modelo.evaluaciones.php',
+'modelos/modelo.informacion.php',
+'modelos/modelo.login.php',
+'modelos/modelo.perfil.php',
+'modelos/modelo.qr.php',
+'modelos/modelo.recuperar.php',
+'modelos/modelo.registro.php',
+'modelos/modelo.reservar.php',
+'modelos/modelo.swipe.php'
 ];
-​
 self.addEventListener('install', function(event) {
     //Precache files
     event.waitUntil(
@@ -26,9 +56,6 @@ self.addEventListener('install', function(event) {
         })
     );
 });
-​
-​
-​
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
