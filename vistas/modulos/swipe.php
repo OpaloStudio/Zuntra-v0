bbbee46b182c7ad299a2ec61227f21682850fb9e
RobertoVz <roberto@opalo.studio>
2019-09-10 20:26:59 -0500

    listo el carousel del swipe

--- vistas/modulos/swipe.php
+++ vistas/modulos/swipe.php
@@ -23,12 +23,46 @@
   </nav>
   
 
-  <div class="row imgSwipe">
-    <div class="col-12">
+  <div class=" carouselSwipe">
     
-    <div class="divNombre">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-3.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-4.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
 
-    
+    <div class="row">
+      <div class="col-12">
+      <div class="divNombre">
+      <h2 id = 'nombreSwipe' >Rodrigo Rosas</h2>
+      <h6>24 años - citas</h6>
+      </div>
+      </div>
+    </div>
+  
+
 
 <!-- Modal -->
 <div class="modal fade elModal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -77,11 +111,6 @@
   </div>
 </div>
 
-    <h2 id = 'nombreSwipe' >Rodrigo Rosas</h2>
-    <h6>24 años - citas</h6>
-    </div>
-    </div>
-  </div>
   <span class="barritaBtn">
       <div class="btnEleccion" id="nelson">
       <i class="fas fa-thumbs-down" onclick='noMatch()'></i>
