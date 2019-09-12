d2282b942cbd52b7f7efb943be0174c02fc87bc8
RobertoVz <roberto@opalo.studio>
2019-09-12 15:14:26 -0500

    listo la carga de imagenes del carousel del swipe automatica

--- vistas/modulos/swipe.php
+++ vistas/modulos/swipe.php
@@ -26,20 +26,22 @@
   <div class=" carouselSwipe">
     
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
+ 
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-1.jpg" alt="First slide">
+      <img class="d-block w-100 imgSwipe" id="imgSwip1" src="" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-3.jpg" alt="Second slide">
+      <img class="d-block w-100 imgSwipe" id="imgSwip2" src="" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100 imgSwipe" src="vistas/img/usuarios/16/16-4.jpg" alt="Third slide">
+      <img class="d-block w-100 imgSwipe" id="imgSwip3" src="" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgSwipe" id="imgSwip4" src="" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgSwipe" id="imgSwip5" src="" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
