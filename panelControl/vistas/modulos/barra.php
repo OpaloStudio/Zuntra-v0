666eb42a307fb0a6fdd086433a04ff72bb832d1e
RobertoVz <roberto@opalo.studio>
2019-09-14 12:53:33 -0500

    camibos en el app y el en panel de control

--- panelControl/vistas/modulos/barra.php
+++ panelControl/vistas/modulos/barra.php
@@ -1,1 +1,14 @@
- 
+<ul class="nav flex-column barra">
+  <li class="nav-item">
+    <a class="nav-link active" href="?page=2">Dashboard</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=3">Rps</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=4">Reservaciones</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="?page=5" >Disabled</a>
+  </li>
+</ul>
