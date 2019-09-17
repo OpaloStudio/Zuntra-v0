80ac4815a019a9dd1d5871b2445439abe4a6fa7c
RobertoVz <roberto@opalo.studio>
2019-09-17 14:31:53 -0500

    terminada la primera seccion del panel de control. Se estan terminando de implementar los iconos

--- panelControl/vistas/modulos/barra.php
+++ panelControl/vistas/modulos/barra.php
@@ -1,14 +1,17 @@
-<ul class="nav flex-column barra">
+<ul class="nav  barra">
   <li class="nav-item">
-    <a class="nav-link active" href="?page=2">Dashboard</a>
+    <a class="nav-link active" href="?page=2"><i class="fas fa-home iconito"></i></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="?page=3">Rps</a>
+    <a class="nav-link" href="?page=3"><i class="fas fa-user-check iconito"></i></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="?page=4">Usuarios</a>
+    <a class="nav-link" href="?page=4"><i class="fas fa-address-book iconito"></i></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link " href="?page=5" >Disabled</a>
+    <a class="nav-link " href="?page=5" ><i class="fas fa-clipboard-list iconito"></i></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="?page=5" ><i class="fas fa-cog iconito"></i></a>
   </li>
 </ul>
