c267e688071ce018a17ef15bc104839bb265e029
Abner <abner@opalo.studio>
2019-10-10 16:02:55 -0500

    conteo de pulseras con fecha

--- controladores/controlador.escaner.php
+++ controladores/controlador.escaner.php
@@ -13,10 +13,29 @@ if(isset($_SESSION['loggedin'])){
 
 ?>
 <script type="text/javascript" src="vistas/js/script.js"></script>
+<script type="text/javascript" src="vistas/js/moment.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-range/4.0.2/moment-range.js"></script>
 <script>
+
+    window['moment-range'].extendMoment(moment);
     var filtrado = false;
     var sesion = <?php echo $idsesion; ?>;
     var tipoUser = <?php echo $tipoUser; ?>;
+    var hoy = new Date();
+    var dia;
+    var mes;
+    var ano;
+    var horas;
+    var minutos;
+
+    var dia1;
+    var dia2;
+    var nuevoDia;
+    var nuevoDia2;
+    var nuevoMes;
+    var nuevoMes2;
+
+
 
     $(document).ready(function () {
 
@@ -38,7 +57,104 @@ if(isset($_SESSION['loggedin'])){
           window.location.href = linkSwipe;
       }
 
+      dia = hoy.getDate();
+      mes = hoy.getMonth()+1;
+      ano = hoy.getFullYear();
+      horas = hoy.getHours();
+      minutos = hoy.getMinutes();
+      
+
+      console.log("Día: "+dia);
+      console.log("Mes: "+mes);
+      console.log("Año: "+ano);
+      console.log("horas: "+horas);
+      console.log("Minutos: "+minutos);
+
+      var now = new Date(ano, mes, dia, horas, minutos);
+      
+      var inicioGeneral = new Date(ano, mes, dia, 21, 30);
+      var finD1 = new Date(ano, mes, dia, 23, 59);
+
+      var inicioD2 = new Date(ano, mes, dia+1, 0, 0);
+      var finGeneral = new Date(ano, mes, dia+1, 2, 59);
+
+      console.log("Hoy " + now);
+      console.log("Inicio General " + inicioGeneral);
+      console.log("Fin General " + finGeneral);
+      console.log("Fin Día 1 " + finD1);
+      console.log("Inicio Día 2 " + inicioD2);
+
+      var rangoGeneral  = moment().range(inicioGeneral, finGeneral);
+
+      var rangoD1  = moment().range(inicioGeneral, finD1);
+      var rangoD2  = moment().range(inicioD2, finGeneral);
+
+
+      if(rangoGeneral.contains(now) == true){
+        console.log("Es hora de escaneo de pulseras");
+        if(rangoD1.contains(now) == true){
+          console.log("Hoy es día 1");
+
+          dia1 = formatDate(now);
+          dia2 = formatDate(now);
+          
+          console.log("Día 1 " + dia1);
+          console.log("Día 2 " + dia2);
+        }else{
+          console.log("Hoy es día 2");
+    
+          dia1 = formatDate3(now);
+          dia2 = formatDate(now);
+          
+          console.log("Día 1 " + dia1);
+          console.log("Día 2 " + dia2);
+        }
+      }else{
+        console.log("No es hora de escaneo de pulseras");
         
+      }
+
+      function formatDate(date) {
+          var d = new Date(date),
+              month = '' + d.getMonth(),
+              day = '' + d.getDate(),
+              year = d.getFullYear();
+
+          if (month.length < 2) 
+              month = '0' + month;
+          if (day.length < 2) 
+              day = '0' + day;
+
+          return [year, month, day].join('-');
+      }
+
+      function formatDate2(date) {
+          var d = new Date(date),
+              month = '' + d.getMonth(),
+              day = '' + (d.getDate()+1),
+              year = d.getFullYear();
+
+          if (month.length < 2) 
+              month = '0' + month;
+          if (day.length < 2) 
+              day = '0' + day;
+
+          return [year, month, day].join('-');
+      }
+      function formatDate3(date) {
+          var d = new Date(date),
+              month = '' + d.getMonth(),
+              day = '' + (d.getDate()-1),
+              year = d.getFullYear();
+
+          if (month.length < 2) 
+              month = '0' + month;
+          if (day.length < 2) 
+              day = '0' + day;
+
+          return [year, month, day].join('-');
+      }
+      
 
     });
 
@@ -56,16 +172,20 @@ if(isset($_SESSION['loggedin'])){
         if(filtrado == false){
 
           if (content == "cover") {
-            
+            console.log(dia1);
+            console.log(dia2);
             option = 1;
   
             $.ajax({
               url: "modelos/modelo.escaner.php",
               type: "POST",
               data: ({
-                  option:option
+                  option:option,
+                  dia1:dia1,
+                  dia2:dia2
               }),
               success: function(msg) {
+                console.log(msg);
                   switch(msg){
   
                       case 999:
@@ -88,9 +208,12 @@ if(isset($_SESSION['loggedin'])){
               url: "modelos/modelo.escaner.php",
               type: "POST",
               data: ({
-                  option:option
+                  option:option,
+                  dia1:dia1,
+                  dia2:dia2
               }),
               success: function(msg) {
+                console.log(msg);
                   switch(msg){
   
                       case 998:
