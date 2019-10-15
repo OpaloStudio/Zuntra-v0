69bc4a5c75fd3672e91013e0ced8f56deb35225d
RobertoVz <roberto@opalo.studio>
2019-10-15 14:09:34 -0500

    mas cambios en configuracion

--- panelControl/vistas/modulos/configuracion.php
+++ panelControl/vistas/modulos/configuracion.php
@@ -79,8 +79,77 @@
                     </form>
 
                 </div>
+<!-- Zona de administrar staff-->
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div class="zonaRps">
+        <div class="wrap flexRps2">
 
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+        <div class="card cardNegra cardRp mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre Monito</h5>
+                                        <p class="card-text">30/50
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card cardNegra cardRp mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="vistas/img/logo.png" class="card-img" alt="profile-pic">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre Monito</h5>
+                                        <p class="card-text">30/50
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card cardNegra cardRp mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="vistas/img/logo.png" class="card-img" alt="profile-pic">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre Monito</h5>
+                                        <p class="card-text">30/50
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card cardNegra cardRp mb-3" style="max-width: 540px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="vistas/img/logo.png" class="card-img" alt="profile-pic">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre Monito</h5>
+                                        <p class="card-text">30/50
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                       
+                        
+        
+        </div>
+    </div>
+                </div>
+
                 <div class="tab-pane fade" id="nav-eventos" role="tabpanel" aria-labelledby="nav-eventos-tab">...</div>
                 <div class="tab-pane fade" id="nav-diseno" role="tabpanel" aria-labelledby="nav-diseno-tab">...</div>
             </div>
