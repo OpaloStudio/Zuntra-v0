8865004f6077e319f97c9761c97a24ae7b940048
Alberto Sanchez Martinez <albersanmar4@gamil.com>
2019-10-24 17:36:10 -0500

    Vista de reservaciones en el panel de control

--- panelControl/controladores/controlador.reservaciones.php
+++ panelControl/controladores/controlador.reservaciones.php
@@ -1,7 +1,38 @@
 <script>
+    $(document).ready(function() {
+        $(".cardTableReserv").niceScroll({cursorcolor:"#DEC9A1"});
+        $('#4').addClass('activeMas'); 
 
-$( document ).ready(function() {
-    $(".cardTableReserv").niceScroll({cursorcolor:"#DEC9A1"});
-    $('#4').addClass('activeMas'); 
-});
+        //Obtener reservaciones
+        $.ajax({
+            type: "post",
+            url: "modelos/modelo.reservaciones.php",
+            data: {
+                "opcion": "1"
+            },
+            success: function(response) {
+                if(response != "0") {
+                    var reservacion = JSON.parse(response);
+                    $("#tablaReservaciones tbody").empty();
+                    for(var i = 0; i < reservacion.length; i++)
+                        $("#tablaReservaciones").append("<tr><th scope='col'>" + (i + 1) + "</th><td>" + reservacion[i].usuario + "</td><td>" + reservacion[i].tipoRes + "</td><td>" + reservacion[i].staff + "</td><td>" + reservacion[i].numPersonas + "</td></tr>");
+                }
+            }
+        });
+
+        //Obtener total de reservaciones
+        $.ajax({
+            type: "post",
+            url: "modelos/modelo.reservaciones.php",
+            data: {
+                "opcion": "2"
+            },
+            success: function(response) {alert(response);
+                if(response != "0") {
+                    var datos = response.split("-");
+                    $("#reservacionesTotales").text(datos[1] + " / " + datos[0]);
+                }
+            }
+        });
+    });
 </script>
--- panelControl/vistas/modulos/reservaciones.php
+++ panelControl/vistas/modulos/reservaciones.php
@@ -2,7 +2,7 @@
     <div class="zonaReservaciones">
         <div class="card cardReservTotales">
             <h2>Reservaciones Totales</h2>
-            <h1>345 / 543</h1>
+            <h1 id="reservacionesTotales">345 / 543</h1>
         </div>
     </div>
 
@@ -13,175 +13,17 @@
                 <input class="form-control mr-sm-2 busquedaRp" type="search" placeholder="Search" aria-label="Search">
             </div>
             <div class="table-responsive flexTable">
-                <table class="table table-hover tablaUsuarios">
+                <table class="table table-hover tablaUsuarios" id="tablaReservaciones">
                     <thead class="headTabla">
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Cumpleaños</th>
-                            <th scope="col">Correo</th>
-                            <th scope="col">Teléfono</th>
-                            <th scope="col">Sexo</th>
-                            <th scope="col">Busca</th>
-                            <th scope="col">Tipo de cita</th>
+                            <th scope="col">Nombre de quién Reserva</th>
+                            <th scope="col">Tipo de reservación</th>
+                            <th scope="col">Reserva con</th>
+                            <th scope="col">Número de personas</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
