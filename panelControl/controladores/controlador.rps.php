df7125222a20670bce33b644e5840ab4d6e96fa9
RobertoVz <roberto@opalo.studio>
2019-10-24 21:48:57 -0500

    cambios en panel de control

--- panelControl/controladores/controlador.rps.php
+++ panelControl/controladores/controlador.rps.php
@@ -14,7 +14,7 @@
                 if(response != "0") {
                     var usuarios = JSON.parse(response);
                     for(var i = 0; i < usuarios.length; i++)
-                        $("#rps").append('<div class="card cardNegra cardRp mb-3" style="max-width: 540px;"><div class="row no-gutters"><div class="col-md-4"><img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic"></div><div class="col-md-8"><div class="card-body"><h5 class="card-title">' + usuarios[i].nombre + '</h5><p class="card-text">30/50</p></div></div></div></div>');
+                        $("#rps").append('<div class="card cardNegra cardRp mb-3" style="max-width: 540px;"><div class="row no-gutters"><div class="col-md-4"><img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic"></div><div class="col-md-5"><div class="card-body"><h5 class="card-title">' + usuarios[i].nombre + '</h5><p class="card-text">30/50</p></div></div> <div class="col-md-3 znBtns"><div class="editar"> <h5 class="dorado text-center">Más Información</h5> </div></div></div></div>');
                 }
             }
         });
