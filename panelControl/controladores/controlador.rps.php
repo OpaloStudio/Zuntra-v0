6dd0f6652bc1fbfca5e9ed27fe72e2208ce5c9f3
Alberto Sanchez Martinez <albersanmar4@gamil.com>
2019-10-26 10:13:18 -0500

    Arreglando bugs de los rps

--- panelControl/controladores/controlador.rps.php
+++ panelControl/controladores/controlador.rps.php
@@ -27,7 +27,7 @@
             type: "post",
             url: "modelos/modelo.rps.php",
             data: {
-                "tipoStaff": staff
+                "tipoStaff": me.value
             },
             success: function(response) {
                 if(response != "0") {
@@ -36,7 +36,7 @@
                     for(var i = 0; i < usuarios.length; i++)
                         $("#rps").append('<div class="card cardNegra cardRp mb-3" style="max-width: 540px;"><div class="row no-gutters"><div class="col-md-4"><img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic"></div><div class="col-md-5"><div class="card-body"><h5 class="card-title">' + usuarios[i].nombre + '</h5><p class="card-text">30/50</p></div></div> <div class="col-md-3 znBtns"><div class="editar"> <h5 class="dorado text-center">Más Información</h5> </div></div></div></div>');
                 } else
-                    alert("No hay usuarios " + staff);
+                    alert("No hay usuarios " + me.value);
             }
         });
     }
