c7f8b627c8fbf934d94efaaf9b5936a2e60e3c28
Alberto Sanchez Martinez <albersanmar4@gamil.com>
2019-10-26 10:43:55 -0500

    Arreglando bugs de los rps en el panel de control

--- panelControl/controladores/controlador.rps.php
+++ panelControl/controladores/controlador.rps.php
@@ -44,10 +44,12 @@
     }
 
     function keyupBuscar(me) {
-        $("#rps").empty();
-        for(var i = 0; i < usuarios.length; i++)
-            if(usuarios[i].nombre.toLowerCase().includes(me.value.toLowerCase()))
-                $("#rps").append('<div class="card cardNegra cardRp mb-3" style="max-width: 540px;"><div class="row no-gutters"><div class="col-md-4"><img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic"></div><div class="col-md-5"><div class="card-body"><h5 class="card-title">' + usuarios[i].nombre + '</h5><p class="card-text">30/50</p></div></div> <div class="col-md-3 znBtns"><div class="editar"> <h5 class="dorado text-center">Más Información</h5> </div></div></div></div>');
+        if(usuarios.length > 0) {
+            $("#rps").empty();
+            for(var i = 0; i < usuarios.length; i++)
+                if(usuarios[i].nombre.toLowerCase().includes(me.value.toLowerCase()))
+                    $("#rps").append('<div class="card cardNegra cardRp mb-3" style="max-width: 540px;"><div class="row no-gutters"><div class="col-md-4"><img src="vistas/img/perfil.jpg" class="card-img" alt="profile-pic"></div><div class="col-md-5"><div class="card-body"><h5 class="card-title">' + usuarios[i].nombre + '</h5><p class="card-text">30/50</p></div></div> <div class="col-md-3 znBtns"><div class="editar"> <h5 class="dorado text-center">Más Información</h5> </div></div></div></div>');
+        }
     }
 </script>
 
